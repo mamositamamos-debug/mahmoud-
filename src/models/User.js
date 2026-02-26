f023753b6b6168b3db855bb916ccdddac0043403
mamositamamos-debug <mamositamamos@gmail.com>
2026-02-26 19:10:02 +0200

    Create User schema in User.js

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profile: {
+        firstName: String,
+        lastName: String,
+        bio: String,
+        avatar: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
